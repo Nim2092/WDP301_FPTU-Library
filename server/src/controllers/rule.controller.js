914dddd65581a7332d05aa12b61a62b72789e32e
NguyenTienDat <luanoutplay@gmail.com>
2024-10-12 20:33:02 +0700

    Add feature to Rule controller and model

--- server/src/controllers/rule.controller.js
+++ server/src/controllers/rule.controller.js
@@ -1,6 +1,112 @@
 const { default: mongoose } = require("mongoose");
 const db = require("../models");
-const { user: User, role: Role, rule: Rule } = db;
+const { user: User, rule: Rule } = db;
 
-const RuleController = {};
+//list rule
+async function listRule(req, res, next) {
+  try {
+    const listRule = await Rule.find();
+
+    if (!listRule) {
+      return res.status(404).send({ message: "Rule not found" });
+    }
+    const newListRule = listRule.map((rule) => ({
+      id: rule.id,
+      title: rule.title,
+      content: rule.content,
+      createdBy: rule.createdBy,
+      updatedBy: rule.updatedBy,
+    }));
+    res.status(200).json({
+      message: "Get list rule successfully",
+      data: newListRule,
+    });
+  } catch (error) {
+    console.error("Error listing rule", error);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+//create a rule
+async function createRule(req, res, next) {
+  try {
+    const { title, content, createdBy, updatedBy } = req.body;
+    if (!title || !content) {
+      return res
+        .status(400)
+        .send({ message: "Title and content are required" });
+    }
+    const rule = new Rule({
+      title: title,
+      content: title,
+      //   createdBy: req.user.id,
+      //   updatedBy: req.user.id,
+      createdBy: createdBy,
+      updatedBy: updatedBy,
+    });
+    const savedRule = await rule.save();
+    res.status(201).json({
+      message: "Rule created successfully",
+      data: savedRule,
+    });
+  } catch (error) {
+    console.error("Error creating rule", error);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+//update a rule with id
+async function updateRule(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { title, content, updatedBy } = req.body;
+
+    if (!title || !content) {
+      return res
+        .status(400)
+        .send({ message: "Title and content are required" });
+    }
+
+    const rule = await Rule.findByIdAndUpdate(
+      id,
+      { title, content, updatedBy: /* req.user.id */ updatedBy },
+      { new: true }
+    );
+    if (!rule) {
+      return res.status(404).send({ message: "Rule not found" });
+    }
+    res.status(200).json({
+      message: "Rule updated successfully",
+      data: rule,
+    });
+  } catch (error) {
+    console.error("Error updating rule", error);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+//delete a rule with id
+async function deleteRule(req, res, next) {
+  try {
+    const { id } = req.params;
+    const rule = await Rule.findByIdAndDelete(id);
+    if (!rule) {
+      return res.status(404).send({ message: "Rule not found" });
+    }
+    res.status(200).json({
+      message: "Rule deleted successfully",
+      data: rule,
+    });
+  } catch (error) {
+    console.error("Error deleting rule", error);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+const RuleController = {
+  listRule,
+  createRule,
+  updateRule,
+  deleteRule,
+};
 module.exports = RuleController;
