64be16a516c075a784bfe5781ae10ca477f312e6
NguyenTienDat <luanoutplay@gmail.com>
2024-10-12 18:04:28 +0700

    Refactor news controller and model to include thumbnail field

--- server/src/models/news.model.js
+++ server/src/models/news.model.js
@@ -5,9 +5,9 @@ const newsSchema = new Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    thumbnail: { type: String, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
     updatedBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    thumbnail: String,
   },
   { timestamps: true }
 );
